e6b611ffd9b80850f4155a91886871a12d7713f6
Yasin Can <Yasin@DESKTOP-K386BOA>
2021-07-16 23:39:59 +0300

    .gitignore

--- Rigel.ViewModels/Error.cs
+++
@@ -1,9 +0,0 @@
-namespace Rigel.ViewModels
-{
-    public class Error
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
